399b7b1de323c83cd3237b976ede45545fae3c1e
arpitchaudhary03 <coolarpit021@gmail.com>
2022-06-01 16:48:13 +0530

    resume added

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/arpit_chaudhary.pdf";
+import pdf from "../../Assets/../Assets/ResumeRound2.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-const resumeLink =
-  "https://raw.githubusercontent.com/arpitchaudhary03/portfolio/master/src/Assets/arpit_chaudhary.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -35,9 +32,9 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
+            {/* <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} /> */}
           </Document>
         </Row>
 
